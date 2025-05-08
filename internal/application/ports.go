fea96313f6c5b02cc235eae6fea047db69c1fade
Toolnado <toolnado@gmail.com>
2025-05-08 21:21:18 +0300

    feat: add func repository interface

--- internal/application/ports.go
+++ internal/application/ports.go
@@ -1,1 +1,13 @@
 package application
+
+import (
+	"context"
+
+	"github.com/Toolnado/go-faas/internal/domain"
+	"github.com/google/uuid"
+)
+
+type IFunctionRepository interface {
+	Insert(ctx context.Context, f *domain.Function) (*domain.Function, error)
+	Get(ctx context.Context, id uuid.UUID) (*domain.Function, error)
+}

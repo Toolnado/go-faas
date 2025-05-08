861c9c56aa3e556958c82940c3adc223149ed6e2
Toolnado <toolnado@gmail.com>
2025-05-08 21:15:43 +0300

    feat: add function struct

--- internal/domain/function.go
+++ internal/domain/function.go
@@ -1,1 +1,27 @@
 package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Function struct {
+	ID      uuid.UUID
+	Name    string
+	Author  string
+	Code    []byte
+	Version int
+	Created time.Time
+}
+
+func NewFunction(name string, author string, code []byte) Function {
+	return Function{
+		ID:      uuid.New(),
+		Name:    name,
+		Author:  author,
+		Code:    code,
+		Version: 0,
+		Created: time.Now(),
+	}
+}

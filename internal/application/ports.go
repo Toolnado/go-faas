a6ce750bd34f114f0188c06825893e17df2c13d9
Toolnado <toolnado@gmail.com>
2025-05-08 21:23:14 +0300

    feat: add cache interface

--- internal/application/ports.go
+++ internal/application/ports.go
@@ -11,3 +11,8 @@ type IFunctionRepository interface {
 	Insert(ctx context.Context, f *domain.Function) (*domain.Function, error)
 	Get(ctx context.Context, id uuid.UUID) (*domain.Function, error)
 }
+
+type ICache interface {
+	Set(ctx context.Context, key string, value []byte)
+	Get(ctx context.Context, key string) []byte
+}
